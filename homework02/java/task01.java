ab11cb4a1677ab599edca59424e011372a97bc40
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-07-05 11:19:28 +0300

    add hw02

---
+++ homework02/java/task01.java
@@ -0,0 +1,38 @@
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+package homework02.java;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class task01 {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name","Ivanov");
+        map.put("country","Russia");
+        map.put("city","Moscow");
+        map.put("age",null);
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> param) {
+        StringBuilder result = new StringBuilder();
+        if (param == null || param.isEmpty())
+            return result.toString();
+
+        for (Map.Entry<String, String> pair : param.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
+    }
+}
